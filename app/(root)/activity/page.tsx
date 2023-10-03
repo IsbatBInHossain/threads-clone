288a36997fa4240636589284cf35d5beb423f45d
Isbat-Bin-Hossain <isbat31416@gmail.com>
2023-10-03 10:47:25 +0600

    Add search, communities and activities page

---
+++ app/(root)/activity/page.tsx
@@ -0,0 +1,4 @@
+const Page = () => {
+  return <h1 className='head-text mb-10'>Activity</h1>
+}
+export default Page
---
+++ app/(root)/search/page.tsx
@@ -0,0 +1,4 @@
+const Page = () => {
+  return <h1 className='head-text mb-10'>Search</h1>
+}
+export default Page
