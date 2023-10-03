f79e4bf4bd7e4694b3c80549848df2ddc96794a8
Isbat-Bin-Hossain <isbat31416@gmail.com>
2023-10-03 11:17:39 +0600

    Complete backend api for fetchUsers

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,4 +1,28 @@
-const Page = () => {
+import ProfileHeader from '@/components/shared/ProfileHeader'
+import { profileTabs } from '@/constants'
+import { fetchUser, fetchUsers } from '@/lib/actions/user.actions'
+import { currentUser } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+import Image from 'next/image'
+import ThreadsTab from '@/components/shared/ThreadsTab'
+
+const SearchPage = async () => {
+  const user = await currentUser()
+  if (!user) return null
+  const userInfo = await fetchUser(user.id)
+
+  // Fetch Users
+
+  const result = await fetchUsers({
+    userId: user.id,
+    searchString: '',
+    pageNumber: 1,
+    pageSize: 25,
+    sortBy: 'desc',
+  })
+
+  if (!userInfo.onboarded) redirect('/onboarding')
+
   return <h1 className='head-text mb-10'>Search</h1>
 }
-export default Page
+export default SearchPage
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -18,6 +18,14 @@ interface UpdateUserParams {
   path: string
 }
 
+interface FetchUsersProps {
+  userId: string
+  searchString?: string
+  pageNumber?: number
+  pageSize?: number
+  sortBy: SortOrder
+}
+
 export async function fetchUser(userId: string) {
   try {
     connectToDB()
@@ -84,5 +92,45 @@ export async function fetchUserPosts(userId: string) {
     return threads
   } catch (error: any) {
     throw new Error(`Error fetching post: ${error.message}`)
+  }
+}
+
+export async function fetchUsers({
+  userId,
+  searchString = '',
+  pageNumber = 1,
+  pageSize = 20,
+  sortBy = 'desc',
+}: FetchUsersProps) {
+  try {
+    connectToDB()
+
+    const skip = (pageNumber - 1) * pageSize
+
+    const regex = new RegExp(searchString, 'i')
+
+    const query: FilterQuery<typeof User> = {
+      id: { $ne: userId },
+    }
+
+    if (searchString.trim() !== '') {
+      query.$or = [{ username: { $regex: regex } }, { name: { $regex: regex } }]
+    }
+
+    const sortOptions = { createdAt: sortBy }
+
+    const usersQuery = User.find(query)
+      .sort(sortOptions)
+      .skip(skip)
+      .limit(pageSize)
+
+    const totalUsersCount = await User.countDocuments(query)
+
+    const users = await usersQuery.exec()
+    const isNext = totalUsersCount > skip + users.length
+
+    return { users, isNext }
+  } catch (error: any) {
+    throw new Error(`Error fetching Users: ${error.message}`)
   }
 }
